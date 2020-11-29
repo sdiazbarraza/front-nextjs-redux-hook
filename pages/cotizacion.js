1e68c4fc639fd804a2fdd2dbad2b55bdb5d337da
sebas <simpdev7@gmail.com>
2020-11-29 02:01:44 -0300

    Post data cotizacion

--- pages/cotizacion.js
+++ pages/cotizacion.js
@@ -1,104 +1,164 @@
-import React, {useEffect, useState} from 'react';  
-import {useSelector, useDispatch} from 'react-redux'; 
-import { useForm, Controller } from "react-hook-form"; 
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useForm, Controller } from "react-hook-form";
 import { inputField, SelectField, SwitchField } from "../utils/inputs";
-import { Button } from "antd";
-import {queryCotizacion} from "../actions/cotizacionAction"; 
-import {getKeyByValue} from "../utils"; 
+import { Button, AutoComplete, Input } from "antd";
+import { queryCotizacion } from "../actions/cotizacionAction";
+import { getDestinos } from "../actions/destinoAction";
+import { getKeyByValue } from "../utils";
 import 'antd/dist/antd.css';
-const Cotizacion = () => {  
-    const dispatch = useDispatch();
-    const { handleSubmit, control, errors, reset } = useForm();
-    const cotizacionList = useSelector(state => state.cotizacionList)
-    const {loadingCotizacion, errorCotizacion, cotizaciones} = cotizacionList;
-    const destinoList = useSelector(state => state.destinoList)
-    const {loadingDestino, errorDestino, destinos} = destinoList;
-    const type = ["Student", "Developer", "other"];
- 
-      const onSubmit = (data) => {
-        console.log(data)
-        setTimeout(() => reset({
-          FirstName:'',
-          LastName:'',
-          Email: '',
-        }), 1000);
-    
-      };
-      useEffect(() => {
-        dispatch(queryCotizacion({}))
-       }, [dispatch])
-return (  
-          <form onSubmit={handleSubmit(onSubmit)}>
-                  <div className='input-group'>
-                    <label className='label'>Firstname</label>
-                    <Controller
-                      as={inputField("FirstName")}
-                      name='FirstName'
-                      control={control}
-                      defaultValue=''
-                      rules={{ required: true }}
-                    />
-                    {errors.FirstName && (
-                      <span className='error'>This field is required</span>
-                    )}
-                  </div>
-                  <div className='input-group'>
-                    <label className='label'>LastName</label>
-                    <Controller
-                      as={inputField("LastName")}
-                      name='LastName'
-                      control={control}
-                      defaultValue=''
-                      rules={{ required: true }}
-                    />
-                    {errors.LastName && (
-                      <span className='error'>This field is required</span>
-                    )}
-                  </div>
-                  <div className='input-group'>
-                    <label className='label'>Email</label>
-                    <Controller
-                      as={inputField("Email")}
-                      name='Email'
-                      control={control}
-                      defaultValue=''
-                      rules={{
-                        pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                        required: true,
-                      }}
-                    />
-                    {errors.Email && (
-                      <span className='error'>Please add a valid email</span>
-                    )}
-                  </div>
-                  <div className='input-group'>
-                    <label className='label'>Type of user</label>
-                    <Controller
-                      as={SelectField(type[0], type)}
-                      name='type'
-                      control={control}
-                      defaultValue={type[0]}
-                      rules={{
-                        required: true,
-                      }}
-                    />
-                  </div>
-                  <div className='input-group'>
-                    <label className='label'>
-                      Want to suscribe to our journal?
-                    </label>
-                    <Controller
-                      as={SwitchField()}
-                      name='subscription'
-                      control={control}
-                      defaultValue={true}
-                    />
-                  </div>
-                  <Button type='primary' htmlType='submit'>
-                    Register
-                  </Button>
-                </form>
-          )  
-};  
-  
+const Cotizacion = () => {
+  const dispatch = useDispatch();
+  const { handleSubmit, control, errors, reset } = useForm();
+  const cotizacionList = useSelector(state => state.cotizacionList)
+  const { loadingCotizacion, errorCotizacion, cotizaciones } = cotizacionList;
+  const destinoList = useSelector(state => state.destinoList)
+  const { loadingDestino, errorDestino, destinos } = destinoList;
+  const [optionsComuna, setOptionsComuna] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const [origenComuna, setOrigenComuna] = useState([]);
+  const [destinoComuna, setDestinoComuna] = useState([]);
+  const type = ["Student", "Developer", "other"];
+
+  const onSubmitData = (params) => {
+    params.origin_id = origenComuna;
+    params.destiny_id = destinoComuna;
+    params.type_of_destiny = "domicilio";
+    params.algorithm = 1;
+    params.algorithm_days = 1;
+    console.log(params);
+    dispatch(queryCotizacion(params));
+  };
+  const onSearch = val => {
+    let filtered = dataSource.filter(
+      obj =>
+        obj.key !== 0 &&
+        obj.value
+          .toString()
+          .toLowerCase()
+          .includes(val)
+    );
+    setOptionsComuna(filtered);
+  };
+  const onSelectOrigen = (val, option) => {
+    setOrigenComuna(option.key);
+  };
+  const onSelectDestino = (val, option) => {
+    setDestinoComuna(option.key);
+  };
+
+  useEffect(() => {
+    dispatch(getDestinos())
+  }, [dispatch])
+  useEffect(() => {
+    if (!loadingDestino) {
+      let _datasource = [];
+      destinos.map(destino =>
+        _datasource.push({
+          key: destino.id,
+          value: destino.name,
+        })
+      );
+      setDataSource(_datasource);
+    }
+  }, [loadingDestino])
+  return (
+    <form onSubmit={handleSubmit(onSubmitData)}>
+      <div className='input-group'>
+        <label className='label'>Largo</label>
+        <Controller
+          as={inputField("Largo")}
+          name='length'
+          control={control}
+          defaultValue=''
+          rules={{ required: true }}
+        />
+        {errors.length && (
+          <span className='error'>This field is required</span>
+        )}
+      </div>
+      <div className='input-group'>
+        <label className='label'>Alto</label>
+        <Controller
+          as={inputField("Alto")}
+          name='height'
+          control={control}
+          defaultValue=''
+          rules={{ required: true }}
+        />
+        {errors.height && (
+          <span className='error'>This field is required</span>
+        )}
+      </div>
+      <div className='input-group'>
+        <label className='label'>Ancho</label>
+        <Controller
+          as={inputField("Ancho")}
+          name='width'
+          control={control}
+          defaultValue=''
+          rules={{ required: true }}
+        />
+        {errors.width && (
+          <span className='error'>This field is required</span>
+        )}
+      </div>
+      <div className='input-group'>
+        <label className='label'>Peso</label>
+        <Controller
+          as={inputField("Peso")}
+          name='weight'
+          control={control}
+          defaultValue=''
+          rules={{ required: true }}
+        />
+        {errors.weight && (
+          <span className='error'>This field is required</span>
+        )}
+      </div>
+      <div className='input-group'>
+        <label className='label'>Origen</label>
+        <Controller
+          as={<AutoComplete
+            options={optionsComuna}
+            onSelect={(val, option) => onSelectOrigen(val, option)}
+            onSearch={onSearch}
+            defaultValue=''
+          >
+            <Input.Search size="medium" placeholder="Busca comuna de origen" />
+          </AutoComplete>}
+          name='origin_id'
+          control={control}
+          rules={{
+            required: true,
+          }}
+        />
+
+      </div>
+      <div className='input-group'>
+        <label className='label'>Destino</label>
+        <Controller
+          as={
+            <AutoComplete
+              options={optionsComuna}
+              onSelect={(val, option) => onSelectDestino(val, option)}
+              onSearch={onSearch}
+              defaultValue=''
+            >
+              <Input.Search size="medium" placeholder="Busca comuna de destino" />
+            </AutoComplete>}
+          name='destiny_id'
+          control={control}
+          rules={{
+            required: true,
+          }}
+        />
+      </div>
+      <Button type='primary' htmlType='submit'>
+        Register
+       </Button>
+    </form>
+  )
+};
+
 export default Cotizacion;
--- pages/destinos.js
+++ pages/destinos.js
@@ -1,30 +1,30 @@
-import React, {useEffect, useState} from 'react';  
-import {useSelector, useDispatch} from 'react-redux';  
-import {getDestinos} from "../actions/destinoAction"; 
-import {getKeyByValue} from "../utils"; 
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getDestinos } from "../actions/destinoAction";
+import { getKeyByValue } from "../utils";
 import { Table, Tag } from 'antd';
 import 'antd/dist/antd.css';
-const Destinos = () => {  
-  
-    const dispatch = useDispatch()
-    const destinoList = useSelector(state => state.destinoList)
-    const {loadingDestino, errorDestino, destinos} = destinoList
-    const [dataSource,setDataSource] =useState([]);
-    const columns = [
-        {
-          title: 'Comuna',
-          dataIndex: 'comuna',
-          key: 'comuna',
-          render: text => <p>{text}</p>,
-        },
-        {
-          title: 'Courriers',
-          dataIndex: 'courriers',
-          key: 'courriers',
-          render: courriers => (
-            <>
-              {
-             courriers.length>0?     
+const Destinos = () => {
+
+  const dispatch = useDispatch()
+  const destinoList = useSelector(state => state.destinoList)
+  const { loadingDestino, errorDestino, destinos } = destinoList
+  const [dataSource, setDataSource] = useState([]);
+  const columns = [
+    {
+      title: 'Comuna',
+      dataIndex: 'comuna',
+      key: 'comuna',
+      render: text => <p>{text}</p>,
+    },
+    {
+      title: 'Courriers',
+      dataIndex: 'courriers',
+      key: 'courriers',
+      render: courriers => (
+        <>
+          {
+            courriers.length > 0 ?
               courriers.map(tag => {
                 let color = 'geekblue';
                 return (
@@ -32,38 +32,39 @@ const Destinos = () => {
                     {tag.toUpperCase()}
                   </Tag>
                 );
-              }):
+              }) :
               <p>No hay</p>
-              
-              }
-            </>
-          ),
-        },
-      ];
-    useEffect(() => {
-        dispatch(getDestinos())
-       }, [dispatch])
-    useEffect(()=>{
-        if(!loadingDestino){
-            let _datasource = [];
-            destinos.map(destino => 
-                    _datasource.push({
-                    key:destino.id,
-                    comuna:destino.name,
-                    courriers:getKeyByValue(destino.couriers_availables,destino.name)})
-            );
-            setDataSource(_datasource);
-        }
-    },[loadingDestino])  
- 
-return (  
+
+          }
+        </>
+      ),
+    },
+  ];
+  useEffect(() => {
+    dispatch(getDestinos())
+  }, [dispatch])
+  useEffect(() => {
+    if (!loadingDestino) {
+      let _datasource = [];
+      destinos.map(destino =>
+        _datasource.push({
+          key: destino.id,
+          comuna: destino.name,
+          courriers: getKeyByValue(destino.couriers_availables, destino.name)
+        })
+      );
+      setDataSource(_datasource);
+    }
+  }, [loadingDestino])
+
+  return (
     <>
-    <h3>Destinos disponibles</h3>
-    {loadingDestino ? "Loading..." : errorDestino ? errorDestino.message : 
+      <h3>Destinos disponibles</h3>
+      {loadingDestino ? "Loading..." : errorDestino ? errorDestino.message :
         <Table dataSource={dataSource} columns={columns} />
-        }
+      }
     </>
-	)  
-};  
-  
+  )
+};
+
 export default Destinos;
